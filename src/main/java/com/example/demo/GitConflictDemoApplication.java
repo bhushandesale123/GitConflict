2b66733e1f716f3056f1e9b06d0f83834ebd4f17
aparnajadhav123 <aparnajdhav5133@gmail.com>
2022-08-16 10:58:32 +0530

    Welcome msg added

--- src/main/java/com/example/demo/GitConflictDemoApplication.java
+++ src/main/java/com/example/demo/GitConflictDemoApplication.java
@@ -8,6 +8,7 @@ public class GitConflictDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitConflictDemoApplication.class, args);
+		String msg="Welcome, Good morning all";
 	}
 
 }
